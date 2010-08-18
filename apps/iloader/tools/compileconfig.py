9ee55ed4cc7d62af9c10b46562dbe7876200aa5e
theseven <theseven@2d50ea51-68f8-42ca-bb2e-f50f654680a9>
2010-08-18 12:08:04 +0000

    Fix compileconfig.py copyright header
    git-svn-id: svn://svn.freemyipod.org@149 2d50ea51-68f8-42ca-bb2e-f50f654680a9

--- apps/iloader/tools/compileconfig.py
+++ apps/iloader/tools/compileconfig.py
@@ -4,20 +4,20 @@
 #    Copyright 2010 TheSeven
 #
 #
-#    This file is part of iLoader.
+#    This file is part of emBIOS.
 #
-#    TheSeven's iBugger is free software: you can redistribute it and/or
+#    emBIOS is free software: you can redistribute it and/or
 #    modify it under the terms of the GNU General Public License as
 #    published by the Free Software Foundation, either version 2 of the
 #    License, or (at your option) any later version.
 #
-#    TheSeven's iBugger is distributed in the hope that it will be useful,
+#    emBIOS is distributed in the hope that it will be useful,
 #    but WITHOUT ANY WARRANTY; without even the implied warranty of
 #    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
 #    See the GNU General Public License for more details.
 #
 #    You should have received a copy of the GNU General Public License along
-#    with iLoader.  If not, see <http://www.gnu.org/licenses/>.
+#    with emBIOS.  If not, see <http://www.gnu.org/licenses/>.
 #
 #
 
