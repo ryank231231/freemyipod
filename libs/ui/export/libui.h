ab62e169ec9bae1f5e4993e08e158e255280d645
theseven <theseven@2d50ea51-68f8-42ca-bb2e-f50f654680a9>
2011-01-23 01:04:08 +0000

    New project: emCORE User Interface Library. For now only supports 24bit to lcd-native dithering.
    git-svn-id: svn://svn.freemyipod.org@490 2d50ea51-68f8-42ca-bb2e-f50f654680a9

---
+++ libs/ui/export/libui.h
@@ -0,0 +1,51 @@
+//
+//
+//    Copyright 2011 TheSeven
+//
+//
+//    This file is part of emCORE.
+//
+//    emCORE is free software: you can redistribute it and/or
+//    modify it under the terms of the GNU General Public License as
+//    published by the Free Software Foundation, either version 2 of the
+//    License, or (at your option) any later version.
+//
+//    emCORE is distributed in the hope that it will be useful,
+//    but WITHOUT ANY WARRANTY; without even the implied warranty of
+//    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+//    See the GNU General Public License for more details.
+//
+//    You should have received a copy of the GNU General Public License along
+//    with emCORE.  If not, see <http://www.gnu.org/licenses/>.
+//
+//
+
+
+#ifndef __LIBUI_H__
+#define __LIBUI_H__
+
+#include "emcorelib.h"
+
+
+#include "../dither.h"
+
+
+/* increase this every time the api struct changes */
+#define LIBUI_API_VERSION 1
+
+/* update this to latest version if a change to the api struct breaks
+   backwards compatibility (and please take the opportunity to sort in any
+   new function which are "waiting" at the end of the function table) */
+#define LIBUI_MIN_API_VERSION 1
+
+/* NOTE: To support backwards compatibility, only add new functions at
+         the end of the structure.  Every time you add a new function,
+         remember to increase LIBUI_API_VERSION. If you make changes to the
+         existing APIs, also update LIBUI_MIN_API_VERSION to current version */
+
+struct libui_api
+{
+    typeof(dither)* dither;
+};
+
+#endif
---
+++ libs/ui/main.c
@@ -0,0 +1,33 @@
+//
+//
+//    Copyright 2011 TheSeven
+//
+//
+//    This file is part of emCORE.
+//
+//    emCORE is free software: you can redistribute it and/or
+//    modify it under the terms of the GNU General Public License as
+//    published by the Free Software Foundation, either version 2 of the
+//    License, or (at your option) any later version.
+//
+//    emCORE is distributed in the hope that it will be useful,
+//    but WITHOUT ANY WARRANTY; without even the implied warranty of
+//    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+//    See the GNU General Public License for more details.
+//
+//    You should have received a copy of the GNU General Public License along
+//    with emCORE.  If not, see <http://www.gnu.org/licenses/>.
+//
+//
+
+
+#include "emcorelib.h"
+#include "export/libui.h"
+
+
+struct libui_api apitable =
+{
+    .dither = dither
+};
+
+EMCORE_LIB_HEADER(0x49554365, LIBUI_API_VERSION, LIBUI_MIN_API_VERSION, NULL, NULL, apitable)
