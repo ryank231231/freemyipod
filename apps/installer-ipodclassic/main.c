dad2e537845381b67acc388bcc0d6449769ecbb8
theseven <theseven@2d50ea51-68f8-42ca-bb2e-f50f654680a9>
2011-02-05 23:48:41 +0000

    Installer for iPod Classic: Adapt to emCORE
    git-svn-id: svn://svn.freemyipod.org@530 2d50ea51-68f8-42ca-bb2e-f50f654680a9

--- apps/installer-ipodclassic/main.c
+++ apps/installer-ipodclassic/main.c
@@ -3,46 +3,53 @@
 //    Copyright 2010 TheSeven
 //
 //
-//    This file is part of emBIOS.
+//    This file is part of emCORE.
 //
-//    emBIOS is free software: you can redistribute it and/or
+//    emCORE is free software: you can redistribute it and/or
 //    modify it under the terms of the GNU General Public License as
 //    published by the Free Software Foundation, either version 2 of the
 //    License, or (at your option) any later version.
 //
-//    emBIOS is distributed in the hope that it will be useful,
+//    emCORE is distributed in the hope that it will be useful,
 //    but WITHOUT ANY WARRANTY; without even the implied warranty of
 //    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
 //    See the GNU General Public License for more details.
 //
 //    You should have received a copy of the GNU General Public License along
-//    with emBIOS.  If not, see <http://www.gnu.org/licenses/>.
+//    with emCORE.  If not, see <http://www.gnu.org/licenses/>.
 //
 //
 
 
-#include "embiosapp.h"
+#include "emcoreapp.h"
+#include "libpng.h"
+#include "libui.h"
 
 
 void main();
-EMBIOS_APP_HEADER("Installer thread", 0x10000, main, 127)
+EMCORE_APP_HEADER("emCORE installer", main, 127)
 
 
-uint16_t lcdbuffer[320 * 240];
-#define BMPIDX_SIDEPANE 0
-#define BMPIDX_WARNING 1
-#define BMPIDX_INSTALLING 2
-#define BMPIDX_FORMATTING 3
-#define BMPIDX_COPYING 4
-#define BMPIDX_FLASHING 5
+extern char background_png[];
+extern uint32_t background_png_size;
+extern char darkener_png[];
+extern uint32_t darkener_png_size;
+extern char disclaimer_png[];
+extern uint32_t disclaimer_png_size;
+extern char actions_png[];
+extern uint32_t actions_png_size;
+extern char f_png_emcorelib[];
+extern char f_ui_emcorelib[];
+extern uint32_t flashscript[];
+extern uint32_t firstinstcost;
+extern uint32_t firstinstscript[];
+extern uint32_t commoncost;
+extern uint32_t commonscript[];
+
 
 struct wakeup eventwakeup;
 volatile int button;
-
-char mallocbuf[0xea0000] __attribute__((aligned(16)));
-tlsf_pool mallocpool;
-
-extern uint32_t _scriptstart;
+volatile int scrollpos;
 
 
 #define SHA1CONFIG    (*((volatile uint32_t*)(0x38000000)))
@@ -96,42 +103,12 @@ void sha1(void* data, uint32_t size, void* hash)
 }
 
 
-void handler(enum button_event eventtype, int which, int value)
+void handler(void* user, enum button_event eventtype, int which, int value)
 {
     if (eventtype == BUTTON_PRESS) button |= 1 << which;
+    if (eventtype == WHEEL_MOVED_ACCEL)
+        scrollpos = MAX(0, MIN(295, scrollpos + value / 8));
     wakeup_signal(&eventwakeup);
-}
-
-void* malloc(size_t size)
-{
-    void* result = tlsf_malloc(mallocpool, size);
-    if (!result && size) panic(PANIC_KILLTHREAD, "Out of memory!");
-    return result;
-}
-
-void* memalign(size_t align, size_t size)
-{
-    void* result = tlsf_memalign(mallocpool, align, size);
-    if (!result && size) panic(PANIC_KILLTHREAD, "Out of memory!");
-    return result;
-}
-
-void* realloc(void* ptr, size_t size)
-{
-    void* result = tlsf_realloc(mallocpool, ptr, size);
-    if (!result && size) panic(PANIC_KILLTHREAD, "Out of memory!");
-    return result;
-}
-
-void free(void* ptr)
-{
-    tlsf_free(mallocpool, ptr);
-}
-
-uint32_t freeret(uint32_t rc, void* ptr)
-{
-    tlsf_free(mallocpool, ptr);
-    return rc;
 }
 
 void mkfat32(struct progressbar_state* progressbar)
@@ -171,7 +148,7 @@ void mkfat32(struct progressbar_state* progressbar)
     memcpy(&((uint8_t*)buf)[0x52], "FAT32   ", 8);
     ((uint16_t*)buf)[0xff] = 0xaa55;
     if (rc = storage_write_sectors_md(0, 0, 1, buf))
-        panicf(PANIC_KILLTHREAD, "Error writing MBR: %08X", rc);
+        panicf(PANIC_KILLTHREAD, "\nError writing MBR: %08X", rc);
     memset(buf, 0, 0x800);
     buf[0] = 0x41615252;
     buf[0x79] = 0x61417272;
@@ -179,21 +156,22 @@ void mkfat32(struct progressbar_state* progressbar)
     buf[0x7b] = 2;
     buf[0x7f] = 0xaa550000;
     if (rc = storage_write_sectors_md(0, 1, 1, buf))
-        panicf(PANIC_KILLTHREAD, "Error writing FSINFO: %08X", rc);
-    progressbar_init(progressbar, 5, 189, 65, 80, 0, 0xdefb, 0x1d, 0, fatsectors);
+        panicf(PANIC_KILLTHREAD, "\nError writing FSINFO: %08X", rc);
+    progressbar_init(progressbar, 15, 304, 135, 159, 0x77ff, 0xe8, 0x125f, 0, fatsectors);
     uint32_t cursect = 0;
     for (i = 0; i < fatsectors; i += 32)
     {
         memset(buf, 0, 0x20000);
         if (!i) memcpy(buf, "\xf8\xff\xff\x0f\xff\xff\xff\xff\xff\xff\xff\x0f", 12);
         if (rc = storage_write_sectors_md(0, reserved + i, MIN(fatsectors - i, 32), buf))
-            panicf(PANIC_KILLTHREAD, "Error writing FAT sectors %d-%d: %08X", i, MIN(fatsectors - 1, i + 31), rc);
+            panicf(PANIC_KILLTHREAD, "\nError writing FAT sectors %d-%d: %08X",
+                   i, MIN(fatsectors - 1, i + 31), rc);
         progressbar_setpos(progressbar, i, false);
     }
     memset(buf, 0, secperclus * 0x1000);
     memcpy(buf, "iPodClassic\x08", 12);
     if (rc = storage_write_sectors_md(0, database, secperclus, buf))
-        panicf(PANIC_KILLTHREAD, "Error writing root directory sectors: %08X", i, rc);
+        panicf(PANIC_KILLTHREAD, "\nError writing root directory sectors: %08X", i, rc);
     free(buf);
     disk_mount(0);
 }
@@ -201,10 +179,6 @@ void mkfat32(struct progressbar_state* progressbar)
 void main(void)
 {
     uint32_t i, j, k, rc;
-    void* bitmapdata[6];
-    uint32_t bitmapsize[6];
-    uint32_t* script;
-#define scriptb ((uint8_t*)script)
     uint32_t dummy;
     struct progressbar_state progressbar;
     bool appleflash;
@@ -214,53 +188,56 @@ void main(void)
     uint8_t* oldnor;
 #define oldnorword ((uint32_t*)oldnor)
 
-    button = 0;
-    wakeup_init(&eventwakeup);
-    button_register_handler(handler);
-    mallocpool = tlsf_create(mallocbuf, sizeof(mallocbuf));
+    cputc(3, '.');
+    struct emcorelib_header* libpng = get_library(0x64474e50, LIBPNG_API_VERSION, LIBSOURCE_RAM_NEEDCOPY, f_png_emcorelib);
+    if (!libpng) panicf(PANIC_KILLTHREAD, "\nCould not load PNG decoder library!");
+    struct libpng_api* png = (struct libpng_api*)libpng->api;
+    cputc(3, '.');
+    struct emcorelib_header* libui = get_library(0x49554365, LIBUI_API_VERSION, LIBSOURCE_RAM_NEEDCOPY, f_ui_emcorelib);
+    if (!libui) panicf(PANIC_KILLTHREAD, "\nCould not load user interface library!");
+    struct libui_api* ui = (struct libui_api*)libui->api;
+    cputc(3, '.');
 
-    script = &_scriptstart;
-    for (i = 0; i < 6; i++)
-    {
-        bitmapsize[i] = *script;
-        bitmapdata[i] = &script[1];
-        script = &script[1 + (bitmapsize[i] >> 2)];
-    }
+    struct png_info* handle = png->png_open(background_png, background_png_size);
+    if (!handle) panicf(PANIC_KILLTHREAD, "\nCould not parse background image!");
+    cputc(3, '.');
+    struct png_rgb* bg = png->png_decode_rgb(handle);
+    if (!bg) panicf(PANIC_KILLTHREAD, "\nCould not decode background image!");
+    png->png_destroy(handle);
+    cputc(3, '.');
+    handle = png->png_open(actions_png, actions_png_size);
+    if (!handle) panicf(PANIC_KILLTHREAD, "\nCould not parse actions image!");
+    cputc(3, '.');
+    struct png_rgba* actions = png->png_decode_rgba(handle);
+    if (!actions) panicf(PANIC_KILLTHREAD, "\nCould not decode actions image!");
+    png->png_destroy(handle);
+    cputc(3, '.');
+    void* framebuf = malloc(290 * 165 * 3);
+    if (!framebuf) panicf(PANIC_KILLTHREAD, "\nCould not allocate frame buffer!");
+    cputc(3, '.');
 
-    void* bmpbuffer = malloc(0x25900);
-    memset(lcdbuffer, 0xff, 0x25800);
-    ucl_decompress(bitmapdata[BMPIDX_SIDEPANE], bitmapsize[BMPIDX_SIDEPANE], bmpbuffer, &dummy);
-    renderbmp(&lcdbuffer[195], bmpbuffer, 320);
-    bool updating = mkdir("/iLoader") == -4;
-    if (!updating)
-    {
-        ucl_decompress(bitmapdata[BMPIDX_WARNING], bitmapsize[BMPIDX_WARNING], bmpbuffer, &dummy);
-        renderbmp(lcdbuffer, bmpbuffer, 320);
-        displaylcd(0, 319, 0, 239, lcdbuffer, 0);
-    }
-    else
-    {
-        ucl_decompress(bitmapdata[BMPIDX_INSTALLING], bitmapsize[BMPIDX_INSTALLING],
-                       bmpbuffer, &dummy);
-        renderbmp(lcdbuffer, bmpbuffer, 320);
-    }
-    backlight_set_fade(32);
-    backlight_set_brightness(177);
-    backlight_on(true);
-
+    disk_unmount(0);
+    bool updating = disk_mount(0);
+    cputc(3, '.');
 
     norbuf = memalign(0x100000, 0x10);
     oldnor = memalign(0x100000, 0x10);
     memset(norbuf, 0xff, 0x100000);
+    cputc(3, '.');
     bootflash_readraw(oldnor, 0, 0x100000);
+    cputc(3, '.');
     if (oldnorword[0x400] == 0x53436667) appleflash = false;
     else
     {
+        updating = false;
         if (oldnorword[0] == 0x53436667) appleflash = true;
-        else panic(PANIC_KILLTHREAD, "Boot flash contents are damaged! "
+        else panic(PANIC_KILLTHREAD, "\nBoot flash contents are damaged! "
                                      "(No SYSCFG found)\n\nPlease ask for help.\n");
     }
     memcpy(&norbuf[0x1000], &oldnor[appleflash ? 0 : 0x1000], 0x1000);
+    cputc(3, '.');
+
+    uint32_t* script = flashscript;
     uint32_t sp = 0;
     uint32_t beginptr = 0x2000;
     uint32_t endptr = 0x100000;
@@ -276,7 +253,7 @@ void main(void)
         switch (file)
         {
             default:
-                data = &scriptb[script[sp++]];
+                data = (void*)(script[sp++]);
                 size = script[sp++];
         }
         if (size)
@@ -284,11 +261,11 @@ void main(void)
             if (align && !(flags & 1))
             {
                 if ((align << 12) < beginptr)
-                    panicf(PANIC_KILLTHREAD, "Error: Align failed! (%02X)", align);
+                    panicf(PANIC_KILLTHREAD, "\nError: Align failed! (%02X)", align);
                 beginptr = align << 12;
             }
             if (endptr - beginptr < size)
-                panicf(PANIC_KILLTHREAD, "Error: Flash is full!");
+                panicf(PANIC_KILLTHREAD, "\nError: Flash is full!");
             uint32_t storesize = size;
             if (flags & 2) storesize |= 0x80000000;
             int offs = 0;
@@ -312,7 +289,7 @@ void main(void)
             if (!(flags & 4))
             {
                 if (dirptr >= 0x1000)
-                    panicf(PANIC_KILLTHREAD, "Error: Directory is full!");
+                    panicf(PANIC_KILLTHREAD, "\nError: Directory is full!");
                 memcpy(&norbuf[dirptr], &script[sp], 8);
                 norbufword[(dirptr >> 2) + 2] = file;
                 norbufword[(dirptr >> 2) + 3] = storesize;
@@ -335,14 +312,51 @@ void main(void)
             }
         }
         else if (!(flags & 4)) sp += 2;
+        cputc(3, '.');
     }
 
     if (!updating)
     {
+        void* darkened = malloc(320 * 240 * 3);
+        if (!darkened) panicf(PANIC_KILLTHREAD, "\nCould not allocate darkened image!");
+        cputc(3, '.');
+        handle = png->png_open(darkener_png, darkener_png_size);
+        if (!handle) panicf(PANIC_KILLTHREAD, "\nCould not parse darkener image!");
+        cputc(3, '.');
+        struct png_rgba* darkener = png->png_decode_rgba(handle);
+        if (!darkener) panicf(PANIC_KILLTHREAD, "\nCould not decode darkener image!");
+        png->png_destroy(handle);
+        cputc(3, '.');
+        ui->blenda(320, 240, 255, darkened, 0, 0, 320, bg, 0, 0, 320, darkener, 0, 0, 320);
+        free(darkener);
+        cputc(3, '.');
+        handle = png->png_open(disclaimer_png, disclaimer_png_size);
+        if (!handle) panicf(PANIC_KILLTHREAD, "\nCould not parse disclaimer image!");
+        cputc(3, '.');
+        struct png_rgba* disclaimer = png->png_decode_rgba(handle);
+        if (!disclaimer) panicf(PANIC_KILLTHREAD, "\nCould not decode disclaimer image!");
+        png->png_destroy(handle);
+        cputc(3, '.');
+
+        button = 0;
+        wakeup_init(&eventwakeup);
+        struct button_hook_entry* hook = button_register_handler(handler, NULL);
+        if (!hook) panicf(PANIC_KILLTHREAD, "\nCould not register button hook!");
+
+        displaylcd(0, 0, 320, 240, darkened, 0, 0, 320);
+        backlight_set_fade(32);
+        backlight_set_brightness(177);
+        backlight_on(true);
+        scrollpos = 0;
+
         while (true)
         {
+            ui->blenda(290, 165, 255, framebuf, 0, 0, 290,
+                       darkened, 15, 50, 320, disclaimer, 0, scrollpos, 290);
+            displaylcd(15, 50, 290, 165, framebuf, 0, 0, 290);
+
             wakeup_wait(&eventwakeup, TIMEOUT_BLOCK);
-            if (button == 2) break;
+            if (button == 0x18) break;
             else if (button == 4)
             {
                 shutdown(false);
@@ -350,36 +364,38 @@ void main(void)
             }
             button = 0;
         }
-        memset(lcdbuffer, 0xff, 0x25800);
-        ucl_decompress(bitmapdata[BMPIDX_SIDEPANE], bitmapsize[BMPIDX_SIDEPANE], bmpbuffer, &dummy);
-        renderbmp(&lcdbuffer[195], bmpbuffer, 320);
-        ucl_decompress(bitmapdata[BMPIDX_INSTALLING], bitmapsize[BMPIDX_INSTALLING],
-                       bmpbuffer, &dummy);
-        renderbmp(lcdbuffer, bmpbuffer, 320);
-        ucl_decompress(bitmapdata[BMPIDX_FORMATTING], bitmapsize[BMPIDX_FORMATTING],
-                        bmpbuffer, &dummy);
-        renderbmp(&lcdbuffer[320 * 36], bmpbuffer, 320);
-        displaylcd(0, 319, 0, 239, lcdbuffer, 0);
+
+        button_unregister_handler(hook);
+        free(darkened);
+        free(disclaimer);
+                
+        ui->blenda(165, 36, 255, framebuf, 0, 0, 165, bg, 77, 100, 320, actions, 0, 0, 165);
+        displaylcd(0, 0, 320, 240, bg, 0, 0, 320);
+        displaylcd(77, 100, 165, 36, framebuf, 0, 0, 165);
+
         mkfat32(&progressbar);
     }
 
-    ucl_decompress(bitmapdata[BMPIDX_COPYING], bitmapsize[BMPIDX_COPYING],
-                   bmpbuffer, &dummy);
-    renderbmp(&lcdbuffer[320 * 72], bmpbuffer, 320);
-    displaylcd(0, 319, 0, 239, lcdbuffer, 0);
-    int status;
+    ui->blenda(165, 36, 255, framebuf, 0, 0, 165, bg, 77, 100, 320, actions, 0, 36, 165);
+    displaylcd(0, 0, 320, 240, bg, 0, 0, 320);
+    displaylcd(77, 100, 165, 36, framebuf, 0, 0, 165);
+    backlight_set_fade(32);
+    backlight_set_brightness(177);
+    backlight_on(true);
+    int cost;
     if (updating)
     {
-        status = script[sp + 3];
-        sp = script[sp + 1] >> 2;
+        cost = commoncost;
+        script = commonscript;
     }
     else
     {
-        status = script[sp + 2] + script[sp + 3];
-        sp += 4;
+        cost = firstinstcost + commoncost;
+        script = firstinstscript;
     }
-    progressbar_init(&progressbar, 5, 189, 101, 116, 0, 0xdefb, 0x1d, 0, status);
-    status = 0;
+    sp = 0;
+    cost = 0;
+    progressbar_init(&progressbar, 15, 304, 135, 159, 0x77ff, 0xe8, 0x125f, 0, cost);
     while (script[sp])
     {
         int fd;
@@ -387,7 +403,7 @@ void main(void)
         switch (script[sp])
         {
             case 1:
-                mkdir(&scriptb[script[sp + 1]]);
+                mkdir((char*)(script[sp + 1]));
                 sp += 2;
                 break;
             case 2:
@@ -403,7 +419,7 @@ void main(void)
                     break;
                 }
             case 3:
-                fd = file_open(&scriptb[script[sp + 1]], O_RDONLY);
+                fd = file_open((char*)(script[sp + 1]), O_RDONLY);
                 if (fd >= 0)
                 {
                     close(fd);
@@ -411,8 +427,8 @@ void main(void)
                     break;
                 }
             case 4:
-                if (script[sp + 2] < 0xfffffffe) data = &scriptb[script[sp + 2]];
-                fd = file_creat(&scriptb[script[sp + 1]]);
+                if (script[sp + 2] < 0xfffffffe) data = (void*)(script[sp + 2]);
+                fd = file_creat((char*)(script[sp + 1]));
                 if (fd >= 0)
                 {
                     write(fd, data, script[sp + 3]);
@@ -421,15 +437,14 @@ void main(void)
                 sp += 4;
                 break;
             default:
-                panic(PANIC_KILLTHREAD, "Bad installation script!");
+                panic(PANIC_KILLTHREAD, "\nBad installation script!");
         }
-        status += script[sp++];
-        progressbar_setpos(&progressbar, status, false);
+        cost += script[sp++];
+        progressbar_setpos(&progressbar, cost, false);
     }
-    ucl_decompress(bitmapdata[BMPIDX_FLASHING], bitmapsize[BMPIDX_FLASHING], bmpbuffer, &dummy);
-    renderbmp(&lcdbuffer[320 * 108], bmpbuffer, 320);
-    displaylcd(0, 319, 0, 239, lcdbuffer, 0);
-    progressbar_init(&progressbar, 5, 189, 162, 178, 0, 0xdefb, 0x1d, 0, 256);
+    ui->blenda(165, 36, 255, framebuf, 0, 0, 165, bg, 77, 100, 320, actions, 0, 72, 165);
+    displaylcd(77, 100, 165, 36, framebuf, 0, 0, 165);
+    progressbar_init(&progressbar, 15, 304, 135, 159, 0x77ff, 0xe8, 0x125f, 0, 256);
     for (i = 0; i < 256; i++)
     {
         bootflash_writeraw(&norbuf[i << 12], i << 12, 1 << 12);
