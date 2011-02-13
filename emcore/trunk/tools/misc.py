0e44f5020c9398a97b8578329546c74c0527e8e1
theseven <theseven@2d50ea51-68f8-42ca-bb2e-f50f654680a9>
2011-02-13 16:48:38 +0000

    misc.py: Replace non-printable characters with escape sequences instead of throwing an exception in the logger
    git-svn-id: svn://svn.freemyipod.org@611 2d50ea51-68f8-42ca-bb2e-f50f654680a9

--- emcore/trunk/tools/misc.py
+++ emcore/trunk/tools/misc.py
@@ -79,12 +79,12 @@ class Logger(object):
                 if majorver() > 2:
                     sys.stdout.write(text)
                 else:
-                    sys.stdout.write(text.encode(sys.stdout.encoding))
+                    sys.stdout.write(text.encode(sys.stdout.encoding, "replace"))
             if target == "stderr":
                 if majorver() > 2:
                     sys.stdout.write(text)
                 else:
-                    sys.stderr.write(text.encode(sys.stderr.encoding))
+                    sys.stderr.write(text.encode(sys.stderr.encoding, "replace"))
             elif target == "file":
                 with open(self.logfile, 'a') as f:
                     f.write(text)
