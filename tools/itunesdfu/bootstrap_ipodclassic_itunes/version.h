25b080433147e8c78a99ac3c3b4e0ab27f99081d
theseven <theseven@2d50ea51-68f8-42ca-bb2e-f50f654680a9>
2011-03-29 16:20:22 +0000

    Fix another 330 occurrences of "emBIOS"
    git-svn-id: svn://svn.freemyipod.org@677 2d50ea51-68f8-42ca-bb2e-f50f654680a9

--- tools/itunesdfu/bootstrap_ipodclassic_itunes/version.h
+++ tools/itunesdfu/bootstrap_ipodclassic_itunes/version.h
@@ -3,20 +3,20 @@
 //    Copyright 2011 TheSeven
 //
 //
-//    This file is part of emBIOS.
+//    This file is part of emCORE.
 //
-//    emBIOS is free software: you can redistribute it and/or
+//    emCORE is free software: you can redistribute it and/or
 //    modify it under the terms of the GNU General Public License as
 //    published by the Free Software Foundation, either version 2 of the
 //    License, or (at your option) any later version.
 //
-//    emBIOS is distributed in the hope that it will be useful,
+//    emCORE is distributed in the hope that it will be useful,
 //    but WITHOUT ANY WARRANTY; without even the implied warranty of
 //    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
 //    See the GNU General Public License for more details.
 //
 //    You should have received a copy of the GNU General Public License along
-//    with emBIOS.  If not, see <http://www.gnu.org/licenses/>.
+//    with emCORE.  If not, see <http://www.gnu.org/licenses/>.
 //
 //
 
